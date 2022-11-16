fb183c8d75c893804e6ff5b43fbd40752e57140e
muraliaari <kunashaari@gmail.com>
2022-11-16 14:03:55 +0530

    Ecommapp

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div>
+        <img style={{heigth:"50%"}} src="https://images.remotehub.com/d42c62669a7711eb91397e038280fee0/original_thumb/ec1eb042.jpg?version=1618112516" className="img-fluid" alt="..."></img>
+    </div>
+  )
+}
+
+export default Banner
